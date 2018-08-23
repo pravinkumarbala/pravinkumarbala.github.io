e2b4a51035a68db89bfd596672690b3b455445b4
pravin <pravin.bala@a-cti.com>
2018-08-23 14:52:22 +0530

    Javascript
    Included

---
+++ app.js
@@ -0,0 +1,10 @@
+document.getElementsById("experienceQuestion") = function(){
+	for (var i = 0; i < 5; i++) {
+	  var btn = document.createElement('button');
+	  btn.appendChild(document.createTextNode('Button ' + i));
+	  btn.addEventListener('click', (function(i) {
+	    return function() { console.log(i); };
+	  })(i));
+	  document.body.appendChild(btn);
+	}
+}
